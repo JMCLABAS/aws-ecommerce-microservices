699033ea0042dd9b3dc7ae33c01b7751ba1bcbe2
Jose Maria Clavijo Basañez <pclavijobasanez@gmail.com>
2026-01-20 14:40:49 +0100

    añadir recurso sqs de prueba

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -281,3 +281,11 @@ resource "aws_ecs_service" "orders_service" {
     assign_public_ip = true
   }
 }
+
+resource "aws_sqs_queue" "cola_prueba" {
+  name                      = "mi-primera-cola-terraform"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
