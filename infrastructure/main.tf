8713b305416a93fe86a623f7398810d5dc1440c9
Jose Maria Clavijo Basañez <pclavijobasanez@gmail.com>
2026-01-30 22:38:46 +0100

    actualizar runtime de lambda a python3.12 por deprecacion de AWS (EOL)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -86,13 +86,13 @@ resource "aws_iam_role_policy" "lambda_policy" {
   })
 }
 
-# La Función Lambda (Tu código Python)
+# La Función Lambda 
 resource "aws_lambda_function" "backend_lambda" {
   filename         = "lambda_function.zip"
   function_name    = "ecommerce-backend-function"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
+  runtime          = "python3.12"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   
   environment {
